696f4985df4e82aae52957aed8a8451a3a7de4d7
Miguel Velez <miguelvelez@mijecu25.com>
2021-02-10 14:38:43 -0500

    added option hotspot code

--- src/main/java/at/favre/tools/dconvert/converters/WebConverter.java
+++ src/main/java/at/favre/tools/dconvert/converters/WebConverter.java
@@ -28,77 +28,77 @@ import java.util.List;
 
 /** Converts and creates css image-set style images */
 public class WebConverter extends APlatformConverter<PostfixDescriptor> {
-  public static final String ROOT_FOLDER = "img";
-  private static final String WEB_FOLDER_NAME = "web";
+    public static final String ROOT_FOLDER = "img";
+    private static final String WEB_FOLDER_NAME = "web";
 
-  public static List<PostfixDescriptor> getWebDescriptors() {
-    List<PostfixDescriptor> list = new ArrayList<PostfixDescriptor>();
-    list.add(new PostfixDescriptor(1, "1x", "-1x"));
-    list.add(new PostfixDescriptor(2f, "2x", "-2x"));
-    return list;
-  }
-
-  @Override
-  public List<PostfixDescriptor> usedOutputDensities(Arguments arguments) {
-    return getWebDescriptors();
-  }
-
-  @Override
-  public PostfixDescriptor originalOutputDensities() {
-    return new PostfixDescriptor(1, "1x", "-1x");
-  }
-
-  @Override
-  public String getConverterName() {
-    return "web-converter";
-  }
-
-  @Override
-  public File createMainSubFolder(
-      File destinationFolder, String targetImageFileName, Arguments arguments) {
-    if (arguments.platform.size() > 1) {
-      destinationFolder =
-          MiscUtil.createAndCheckFolder(
-              new File(destinationFolder, WEB_FOLDER_NAME).getAbsolutePath(), arguments.dryRun);
+    public static List<PostfixDescriptor> getWebDescriptors() {
+        List<PostfixDescriptor> list = new ArrayList<PostfixDescriptor>();
+        list.add(new PostfixDescriptor(1, "1x", "-1x"));
+        list.add(new PostfixDescriptor(2f, "2x", "-2x"));
+        return list;
     }
-    return MiscUtil.createAndCheckFolder(
-        new File(destinationFolder, ROOT_FOLDER).getAbsolutePath(), arguments.dryRun);
-  }
 
-  @Override
-  public File createFolderForOutputFile(
-      File mainSubFolder,
-      PostfixDescriptor density,
-      Dimension dimension,
-      String targetFileName,
-      Arguments arguments) {
-    return mainSubFolder;
-  }
-
-  @Override
-  public String createDestinationFileNameWithoutExtension(
-      PostfixDescriptor density, Dimension dimension, String targetFileName, Arguments arguments) {
-    return targetFileName + density.postFix;
-  }
-
-  @Override
-  public void onPreExecute(
-      File dstFolder,
-      String targetFileName,
-      List<PostfixDescriptor> densityDescriptions,
-      ImageType imageType,
-      Arguments arguments)
-      throws Exception {}
-
-  @Override
-  public void onPostExecute(Arguments arguments) {}
-
-  @Override
-  public void clean(Arguments arguments) {
-    if (arguments.platform.size() == 1) {
-      MiscUtil.deleteFolder(new File(arguments.dst, ROOT_FOLDER));
-    } else {
-      MiscUtil.deleteFolder(new File(new File(arguments.dst, WEB_FOLDER_NAME), ROOT_FOLDER));
+    @Override
+    public List<PostfixDescriptor> usedOutputDensities(Arguments arguments) {
+        return getWebDescriptors();
     }
-  }
+
+    @Override
+    public PostfixDescriptor originalOutputDensities() {
+        return new PostfixDescriptor(1, "1x", "-1x");
+    }
+
+    @Override
+    public String getConverterName() {
+        return "web-converter";
+    }
+
+    @Override
+    public File createMainSubFolder(
+            File destinationFolder, String targetImageFileName, Arguments arguments) {
+        if (arguments.platform.size() > 1) {
+            destinationFolder =
+                    MiscUtil.createAndCheckFolder(
+                            new File(destinationFolder, WEB_FOLDER_NAME).getAbsolutePath(), arguments.dryRun);
+        }
+        return MiscUtil.createAndCheckFolder(
+                new File(destinationFolder, ROOT_FOLDER).getAbsolutePath(), arguments.dryRun);
+    }
+
+    @Override
+    public File createFolderForOutputFile(
+            File mainSubFolder,
+            PostfixDescriptor density,
+            Dimension dimension,
+            String targetFileName,
+            Arguments arguments) {
+        return mainSubFolder;
+    }
+
+    @Override
+    public String createDestinationFileNameWithoutExtension(
+            PostfixDescriptor density, Dimension dimension, String targetFileName, Arguments arguments) {
+        return targetFileName + density.postFix;
+    }
+
+    @Override
+    public void onPreExecute(
+            File dstFolder,
+            String targetFileName,
+            List<PostfixDescriptor> densityDescriptions,
+            ImageType imageType,
+            Arguments arguments)
+            throws Exception {}
+
+    @Override
+    public void onPostExecute(Arguments arguments) {}
+
+    @Override
+    public void clean(Arguments arguments) {
+        if (arguments.platform.size() == 1) {
+            MiscUtil.deleteFolder(new File(arguments.dst, ROOT_FOLDER));
+        } else {
+            MiscUtil.deleteFolder(new File(new File(arguments.dst, WEB_FOLDER_NAME), ROOT_FOLDER));
+        }
+    }
 }
